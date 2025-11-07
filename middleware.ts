7b41253e7e6fc1eb8ce099a8df69ef50ffe88294
geeo-1234 <anshu.intern@geoplanetsolution.com>
2025-11-07 23:45:38 +0530

    Fix Clerk Edge middleware import

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,17 @@
-import {clerkMiddleware} from  '@clerk/nextjs/server';
-export default clerkMiddleware();
+import { withClerkMiddleware } from '@clerk/nextjs/dist/edge';
+import { NextResponse } from 'next/server';
+
+export default withClerkMiddleware((req) => {
+  return NextResponse.next();
+});
+
+// Required for Edge
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)'], // Adjust routes as needed
+};
+
+
+
+
 
 
