dc6ce03ca1b2b84e06318bb92604517e4fe7cb05
geeo-1234 <anshu.intern@geoplanetsolution.com>
2025-11-07 19:00:41 +0530

    Initial commit

---
+++ gocart-main/app/api/Inngest/Route.js
@@ -0,0 +1,14 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/inngest/client";
+import { syncUserDeletion } from "./function";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
+
